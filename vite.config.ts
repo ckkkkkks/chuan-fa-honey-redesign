67d02412eee1460c4f048dde2386ec6cd40d7bee
ckkkkkks <160807299+ckkkkkks@users.noreply.github.com>
2025-12-11 22:55:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chuan-fa-honey-redesign/',
   plugins: [react()],
+  // 這裡非常重要，這行會修正 404 路徑錯誤
+  base: '/chuan-fa-honey-redesign/', 
 })
